84988c6809f03089879d1b6665e028cb4375cf47
asde2016 <48622922+asde2016@users.noreply.github.com>
2021-03-31 21:17:11 +0900

    Add files via upload
    gurumare

---
+++ gurumare/js/language.js
@@ -0,0 +1,374 @@
+const lang = {
+    "ENG" : {
+        en : "KOR"
+    },
+    "소극장소개" : {
+        en : "Introduction"
+    },
+    "구름아래소극장" : {
+        en : "Gurumare Small Theater"
+    },
+    "CI 소개" : {
+        en : "CI Info"
+    },
+    "부대시설" : {
+        en : "Facility"
+    },
+    "시설안내" : {
+        en : "Facility"
+    },
+    "무대" : {
+        en : "Stage"
+    },
+    "음향" : {
+        en : "Stage"
+    },
+    "조명" : {
+        en : "Acoustic"
+    },
+    "좌석" : {
+        en : "Seat"
+    },
+    "대기실" : {
+        en : "Waiting Room"
+    },
+    "로비&티켓박스" : {
+        en : "Lobby & TicketBox"
+    },
+    "대관안내" : {
+        en : "Rental"
+    },
+    "하루" : {
+        en : "One day"
+    },
+    "팬미팅&쇼케이스" : {
+        en : "FanMeeting & ShowCase"
+    },
+    "강연&세미나" : {
+        en : "Lecture & Seminar"
+    },
+    "준비" : {
+        en : "Ready"
+    },
+    "기업행사" : {
+        en : "Events"
+    },
+    "촬영" : {
+        en : "Shooting"
+    },
+    "공연안내" : {
+        en : "Show"
+    },
+    "공지사항" : {
+        en : "Notice"
+    },
+    "대관공고" : {
+        en : "Rental"
+    },
+    "이용안내" : {
+        en : "Use Info"
+    },
+    "기타공지" : {
+        en : "Other"
+    },
+    "오시는길" : {
+        en : "Come"
+    },
+    "문화트렌드를 이끄는" : {
+        en : "Leading Cultural Trends"
+    },
+    "다양한 문화콘텐츠로 일상 속 특별함을 주는 환벽한 공간" : {
+        en : "A welcoming space that gives specialness to everyday life<br> with various cultural contents"
+    },
+    "자유로운 형식과 내용의 작품을 담아낼수있는" : {
+        en : "That can contain works of free form and content"
+    },
+    "기획공연과 실험적인 창작공연을 통해 소극장의\n구형화된 이미지를 탈피하고, 차별화된 젊은\n신진 아티스트를 육성하고 지원하는 것을 지향하고 있습니다." : {
+        en : "We aim to cultivate and support differentiated young and<br> young artists by breaking away from the spherical image of<br> small theaters through planned performances and experimental creative performances."
+    },
+    "사이트맵" : {
+        en : "Site Map"
+    },
+    "구름아래소극장의\n다양한 소식을 확인해보세요." : {
+        en : "Check out the various<br> news of the small theater<br> under the clouds."
+    },
+    "[모집] 구름아래소극장 2021년 1분기 대관 공고" : {
+        en : "[Recruitment] Gurumare Small Theater rental announcement in the first quarter of 2021"
+    },
+    "대관기간 2021년 1월 2일 ~ 3월 31일 신청기간 상시모집(문의 02-3280-880) 필수사항 좌석사용 대관시 좌석거리두기 적용(최대 87석)" : {
+        en : "Rental period Jan. 2, 2021 ~ Mar. 31, 2021 Application period Regular recruitment (inquiry 02-3280-880) Mandatory use of seats Seat distance applied when rental (maximum 87 seats)"
+    },
+    "[종료] 2020년 10월~12월 대관 공지" : {
+        en : "[End] Announcement of rental from October to December 2020"
+    },
+    "대관기간 2020년 7월, 8월, 9월 신청기간 상시모집 대관문의 02-3280-8800 필수사항 지그재그 좌석거리두기 적용 : 총 87석" : {
+        en : "Rental period July, August, and September 2020 Application period Regular admission Inquiries 02-3280-8800 Required information Zigzag seating distance applied: 87 seats in total"
+    },
+    "[종료] 2020년 6월 대관 공지" : {
+        en : "[End] Announcement of rental in June 2020"
+    },
+    "대관기간 6월 중 평일(월~목) 대관시간 총 6시간 이용(13:00~22:00에서 협의) 러닝타임 최대 60분 모집내용 공연, 쇼케이스, 음감회, 토크쇼 등 온라인 생중계" : {
+        en : "Rental period: Weekdays (Mon-Thursday) in June, use a total of 6 hours of rental time (consultation from 13:00 to 22:00) Running time Up to 60 minutes Recruitment Contents Performances, showcases, music sensations, talk shows, etc. Online live broadcast"
+    },
+    "[임시휴관] 코로나19로 인한 휴관 2차 연장 안내" : {
+        en : "[Temporary closing] Notice of the 2nd extension of closing due to Corona 19"
+    },
+    "코로나19로 인한 사회적 거리두기가 5월 5일로 연장됨에 따라 구름아래소극장은 임시 휴관 기간을 더 가지게 되었습니다. 휴관 기간에도 구름아래소극장은 정기적인 소독을 실시할 예정입니다. 정상 운영 일정은 아직 미정이며, 정부의 방침을 주시하면서 안전한 환경에서 공연이 가능하다고 판단될 때 다시 공지하겠습니다. 대관 혹은 공연 관련 문의는 02-3280-8800 에서 가능합니다. 감사합니다." : {
+        en : "As the social distancing due to Corona 19 has been extended to May 5, the small theater under Cloud has a temporary closing period. Even during the closed period, the small theater under the cloud is scheduled to undergo regular disinfection. The normal operation schedule is still undecided and will be announced again when it is determined that the performance is possible in a safe environment while keeping an eye on the government's policy. Inquiries regarding rentals or performances can be made at 02-3280-8800. Thank you."
+    },
+    "[임시휴관] 코로나19로 인한 휴관 1차 연장 안내" : {
+        en : "[Temporary closing] Notice of the 1st extension of closing due to Corona 19"
+    },
+    "코로나19 감염 확산을 막기위해 정부에서 다중이용시설 이용 자제를 권고함에 따라, 구름아래소극장은 4월 30일까지 휴관 기간을 연장하게 되었습니다. 사회적 거리두기 실천을 위한 ‘공연장 잠시멈춤' 캠페인에 동참하여 적극적으로 감염을 차단하기 위함이니, 공연 관계자 및 관객 여러분들의 너른 양해 부탁드립니다. 이 기간동안 구름아래소극장은 무관객으로 할 수 있는 다양한 소통거리를 고민하고 있습니다.곧 홈페이지와 인스타그램을 통해 공지하겠습니다." : {
+        en : "In order to prevent the spread of Corona 19 infection, we will comply with the refrain from using multi-use facilities, and the small theater under the clouds will be closed until April 30. This is to actively block infection by participating in the “Performance Pause” campaign to practice social distancing, so we ask for your patience and understanding from the performance officials and all of you. During this period, the small theater under the clouds is contemplating various communication distances that can be made without audiences. We will announce it through the homepage and Instagram soon."
+    },
+    "코로나19로 인한 3월 공연 연기 & 안전점검 안내" : {
+        en : "Postponement of performance in March due to Corona 19 & Notice of Safety Inspection"
+    },
+    "코로나19 위기경보가 심각 단계로 격상됨에 따라 불특정 다수가 모이는 공연장 내 감염을 예방하기 위해 3월 22일(일)까지 예정된 공연을 잠정 연기하게 되었습니다." : {
+        en : "As the Corona 19 crisis warning has been upgraded to a serious stage, the performance scheduled until March 22 (Sun) has been temporarily postponed to prevent infection in the venue where an unspecified crowd gathers."
+    },
+    "개관 1주년 기념 대관지원 이벤트 선정팀 발표" : {
+        en : "Announcement of the selection team for the rental support event celebrating the 1st anniversary"
+    },
+    "신인/경력/랜선라이브/평일/제안 총 5개 부문으로 진행된 이번 이벤트에 예상보다 많은 관심을 주셔서 감사합니다. 그러나 아쉽게도 평일/제안 2개 부문에는 지원팀이 없었습니다. 2개 부문 공석으로 고민한 결과, 무대 기회가 필요한 신인뮤지션 부문에 1팀을 더 선정하게 되었습니다." : {
+        en : "Rookie/Career/Lansun Live/Weekday/Suggestions Thank you for your interest in this event more than expected. Unfortunately, there was no support team in the two categories on weekdays/suggestions. As a result of considering the vacancy in two categories, one more team was selected for the new musicians who needed stage opportunities."
+    },
+    "구름아래소극장 개관 1주년 이벤트" : {
+        en : "The 1st anniversary event of the opening of the small theater under the clouds"
+    },
+    "문화트렌드를 이끄는 홍대 앞, 다양한 문화 콘텐츠로 일상 속 특별함을 주는 완벽한 공간 구름아래소극장이 개관 1주년을 맞이 했습니다." : {
+        en : "In front of Hongdae, which leads cultural trends, the perfect space under the clouds, a perfect space that gives a special look to everyday life with various cultural contents, celebrated its 1st anniversary."
+    },
+    "구름아래소극장 업무 파트별 담당자 안내" : {
+        en : "Information on the person in charge for each part of the small theater under the cloud"
+    },
+    "구름아래소극장 파트별 담당자 연락망입니다. 궁금한 내용은 각 담당자에게 문의 시 빠른 답변 가능합니다. • 공통번호 : 02-3280-8800 • 음향 : 박재현 감독 sound@gurumare.co.kr / 010-9217-9938" : {
+        en : "This is the contact network for each part of the small theater under the clouds. Inquiries can be quickly answered by inquiries to each person in charge. • Common number: 02-3280-8800 • Sound: Director Park Jae-hyun sound@gurumare.co.kr / 010-9217-9938"
+    },
+    "문화트렌드를 이끄는 구름아래소극장" : {
+        en : "Leading Cultural Trends <br><span>Gurumare Small Theater</span>"
+    },
+    "문화트렌드를 이끄는 홍대 앞, 다양한 문화콘텐츠로\n일상 속 특별함을 주는 완벽한 공간\n구름아래소극장은\n무대 3면을 감싸는 LED 백월로 다양한 연출이\n가능하고,\n사석 없는 계단식 좌석으로 최적의 관람이\n가능한 200석 규모의 공연장입니다." : {
+        en : "In front of Hongdae, which leads the cultural trend, a perfect space that gives specialness to everyday life <br class='br1'>with various cultural contents. The small theater under the clouds is capable of various productions\n with LED backwalls that<br class='br1'> wrap around the three sides of the stage, and the optimal viewing is possible with stepped seats without sandstones. It is a seating performance hall."
+    },
+    "INTRODUCTION" : {
+        en : "INTRODUCTION"
+    },
+    "CI" : {
+        en : "CI"
+    },
+    "FACILITIES" : {
+        en : "FACILITIES"
+    },
+    "LOCATION" : {
+        en : "LOCATION"
+    },
+    "문화트렌드를 이끄는\n구름아래소극장을 소개합니다." : {
+        en : "Introducing the gurumare small theater<br> that leads the cultural trend."
+    },
+    "구름아래소극장의\nCI를 소개합니다." : {
+        en : "Introducing the CI of<br> gurumare small theater"
+    },
+    "구름아래소극장의\n부대시설을 소개합니다" : {
+        en : "Introducing the subsidiary facilities of<br> the gurumare small theater"
+    },
+    "구름아래소극장에\n오시는 길을 소개합니다." : {
+        en : "Introducing the directions to the<br> gurumare small theater"
+    },
+    "동일한 대관 일정 문의 시,\n대관신청서 접수 순서대로 검토됩니다." : {
+        en : "When inquiring about the same rental schedule,<br> the rental application will be reviewed in the order of receipt."
+    },
+    "평일(월~목) 하루" : {
+        en : "Weekday(Mon~Thurs)<br> <span>One day</span>"
+    },
+    "주말(금~일,공휴일) 하루" : {
+        en : "Weekend(Fri~Sun,Holiday)<br> <span>One day</span>"
+    },
+    "평일(월~목) 팬미팅&쇼케이스" : {
+        en : "Weekday(Mon~Thurs)<br> <span>FanMeeting & ShowCase</span>"
+    },
+    "주말(금~일,공휴일) 팬미팅&쇼케이스" : {
+        en : "Weekend(Fri~Sun,Holiday)<br> <span>FanMeeting & ShowCase</span>"
+    },
+    "평일(월~목) 강연&세미나" : {
+        en : "Weekday(Mon~Thurs)<br> <span>Lecture & Seminar</span>"
+    },
+    "주말(금~일,공휴일) 강연&세미나" : {
+        en : "Weekend(Fri~Sun,Holiday)<br> <span>Lecture & Seminar</span>"
+    },
+    "평일(월~목) 준비" : {
+        en : "Weekday(Mon~Thurs)<br> <span>Ready</span>"
+    },
+    "주말(금~일,공휴일) 준비" : {
+        en : "Weekend(Fri~Sun,Holiday)<br> <span>Ready</span>"
+    },
+    "평일(월~목) 기업행사" : {
+        en : "Weekday(Mon~Thurs)<br> <span>Events</span>"
+    },
+    "주말(금~일,공휴일) 기업행사" : {
+        en : "Weekend(Fri~Sun,Holiday)<br> <span>Events</span>"
+    },
+    "평일(월~목) 촬영" : {
+        en : "Weekday(Mon~Thurs)<br> <span>Shooting</span>"
+    },
+    "주말(금~일,공휴일) 촬영" : {
+        en : "Weekend(Fri~Sun,Holiday)<br> <span>Shooting</span>"
+    },
+    "01. 무대 LED 일부(좌,중앙,우) 이용" : {
+        en : "01. Using some stage LEDs (left, center, right)"
+    },
+    "무대 LED 일부(좌,중앙,우) 이용" : {
+        en : "Using some stage LEDs (left, center, right)"
+    },
+    "무대 LED 전체(좌,중앙,우) 이용" : {
+        en : "Using all stage LEDs (left, center, right)"
+    },
+    "01. 무대 LED 전체(좌,중앙,우) 이용" : {
+        en : "01. Using all stage LEDs (left, center, right)"
+    },
+    "원" : {
+        en : "won"
+    },
+    "02. 무대 LED 일부(좌,우) 이용" : {
+        en : "02. Using some stage LEDs (left, right)"
+    },
+    "02. 무대 LED 전체(좌,우) 이용" : {
+        en : "02. Using all stage LEDs (left, right)"
+    },
+    "11:00 - 23:00 에서 10시간 이용 가능" : {
+        en : "Available for 10 hours from 11:00-23:00"
+    },
+    "10:30 - 22:30 에서 10시간 이용 가능" : {
+        en : "Available for 10 hours from 10:30-22:30"
+    },
+    "09:00 - 22:00 에서 5시간 이용 가능" : {
+        en : "Available for 5 hours from 09:00-22:00"
+    },
+    "5시간 이용 가능 (*시간협의)" : {
+        en : "Available for 5 hours (*Time agreement)"
+    },
+    "10:00 - 20:00 에서 9시간 이용 가능" : {
+        en : "Available for 9 hours from 10:00-20:00"
+    },
+    "10:00 - 20:00 에서 9시간 이용 가능 (*협의필요)" : {
+        en : "Available for 9 hours from 10:00-20:00 (*Time agreement)"
+    },
+    "9:00 - 23:00 에서 이용 가능 (기본 3시간부터 대관 가능, *시간당 가격)" : {
+        en : "Available from 9:00-23:00 (basic rental available from 3 hours, *Hourly price)"
+    },
+    "하루 대관시 이용가능" : {
+        en : "Available for one day rental"
+    },
+    "촬영이 메인인 대관(드라마,영화,예능 등)" : {
+        en : "The main hall for filming (drama, movie, entertainment, etc.)"
+    },
+    "구름아래소극장의 부대시설을 소개합니다." : {
+        en : "Introducing the subsidiary facilities<br> of the Gurumare small theater"
+    },
+    "구름아래스튜디오" : {
+        en : "GurumareStudio"
+    },
+    "구름아래 스튜디오는\n사전 예약제로 운영됩니다.\n예약 및 문의는 아래번호로 연락주세요." : {
+        en : "The GurumareStudio is operated<br> by reservation in advance.<br> For reservations and inquiries,<br> please contact the number below."
+    },
+    "문의 02-3280-8800 (내선번호 1)" : {
+        en : "Inquiries 02-3280-8800 (extension number 1)"
+    },
+    "구름속에커피집" : {
+        en : "GurumareCoffee"
+    },
+    "영업시간 10:50AM ~ 09:50PM\n문의 02-3280-8800 (내선번호 3)" : {
+        en : "Business hours 10:50AM ~ 09:50PM<br> Inquiries 02-3280-8800 (extension number 3)"
+    },
+    "*구름아래소극장 당일 티켓소지자 10%할인\n*구름아래소극장 당일 공연팀 15% 할인" : {
+        en : "*10% discount for ticket holders<br> on the day of the Gurumare small theater<br>*15% discount for performance teams<br> on the day of Gurumare small theater"
+    },
+    "구름속에백반집" : {
+        en : "GurumareKoreanMeal"
+    },
+    "영업시간 11:30AM ~ 09:50PM\n(브레이크 타임 3:00PM ~ 5:00PM)\n문의 02-3280-8800 (내선번호 4)" : {
+        en : "Business hours 10:50AM ~ 09:50PM<br>(break time 3:00PM ~ 5:00PM)<br> Inquiries 02-3280-8800 (extension number 3)"
+    },
+
+
+
+
+
+
+
+
+
+    "단독 공연[ Tutorial：튜토리얼 ]" : {
+        en : "Solo performance<br>[ Tutorial ]"
+    },
+    "최정윤" : {
+        en : "Jeongyoon Choi"
+    },
+    "앨범 발매 기념 콘서트< 넌 나에게 >" : {
+        en : "Album release commemorative concert<br> &lt you to me &gt"
+    },
+    "기프트(GIFT)" : {
+        en : "GIFT"
+    },
+    "Monthly SOVAC[우리, 같이 할까요?]" : {
+        en : "Monthly SOVAC[Shall we do it together?]"
+    },
+    "이욱정 PD, 김범준 교수, 김형수 대표" : {
+        en : "wookjung Lee PD, Professor beomjoon Kim, and<br> hyungsoo Kim CEO"
+    },
+    "최진기에게 듣는 포스트 코로나" : {
+        en : "Post Corona from<br> jingi Choi"
+    },
+    "최진기" : {
+        en : "jingi Choi"
+    },
+    "라이브 세미나[2020 수능 수학 선택과목 가이드]" : {
+        en : "Live Seminar<br>[2020 SAT Math Elective Course Guide]"
+    },
+    "장예원, 정승제, 이미지, 고정민, 차현우, 김병진" : {
+        en : "yewon Jang, seungje Jeong, miji Lee,<br> jeongmin Go, hyunwoo Cha, byeongjin Kim"
+    },
+    "소리엘 장혁재, 대니정, 이세준, 리사 등" : {
+        en : "Sori L. Jang Hyukjae,<br> Danny Jung, Lee Sejun,<br> Lisa, etc."
+    },
+    "ADDRESS : 서울시 마포구 와우산로29가길 15" : {
+        en : "ADDRESS: 15, Wausan-ro 29ga-gil, Mapo-gu, Seoul"
+    },
+    "구름아래소극장서울시 마포구 와우산로29가길 15" : {
+        en : "Gurumare small theater<span></span> 15, Wausan-ro 29ga-gil, Mapo-gu, Seoul"
+    },
+    "개인정보처리방침" : {
+        en : "Privacy Policy"
+    },
+    "이메일무단수집거부" : {
+        en : "No e-mail collection"
+    },
+    "Rental" : {
+        ko : "대관공고"
+    },
+    "Use Info" : {
+        ko : "이용안내"
+    },
+    "Other" : {
+        ko : "기타공지"
+    },
+    "One day" : {
+        ko : "하루"
+    },
+    "FanMeeting & ShowCase" : {
+        ko : "팬미팅&쇼케이스"
+    },
+    "Lecture & Seminar" : {
+        ko : "강연&세미나"
+    },
+    "Ready" : {
+        ko : "준비"
+    },
+    "Events" : {
+        ko : "기업행사"
+    },
+    "Shooting" : {
+        ko : "촬영"
+    },
+};
